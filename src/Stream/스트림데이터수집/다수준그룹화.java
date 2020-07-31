6e59aa26167f7fb3b2ad1ef67079ba2c9ae2a88a
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-07-31 16:44:23 +0900

    분할의 장점 / example 3개

--- src/Stream/스트림데이터수집/다수준그룹화.java
+++ src/Stream/스트림데이터수집/다수준그룹화.java
@@ -4,10 +4,7 @@ package Stream.스트림데이터수집;
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import org.w3c.dom.ls.LSOutput;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import static java.util.stream.Collectors.*;
 
@@ -44,6 +41,45 @@ public class 다수준그룹화 {
                         maxBy(Comparator.comparingInt(Dish::getCalories)),
                         Optional::get
                 )));
+
+        //groupingBy와 함께 사용하는 다른 컬렉터 예제 (타입별 칼로리의 합 구하기)
+        Map<Dish.Type, Integer> sumCal = Dish.menu.stream().collect(groupingBy(Dish::getType, summingInt(Dish::getCalories)));
+        System.out.println(sumCal);
+
+
+        //채식요리 구하기
+        Map<Boolean, List<Dish>> vegetarian =Dish.menu.stream().collect(partitioningBy(Dish::isVegetarian));
+        List<Dish> vegetarian2 = Dish.menu.stream().filter(d->d.isVegetarian()).collect(toList());
+
+        System.out.println(vegetarian);
+        System.out.println(vegetarian2);
+
+        //채식,비채식 요리를 또 타입별로 분할
+        Map<Boolean, Map<Dish.Type, List<Dish>>> vegetarianDishesByType = Dish.menu.stream().collect(partitioningBy(Dish::isVegetarian, groupingBy(
+                Dish::getType
+        )));
+
+        System.out.println(vegetarianDishesByType);
+
+
+
+        //채식,비채식 나누고 타입별로 요리 그룹화하기
+        Map<Boolean, Map<Dish.Type , List<Dish>>> q1 = Dish.menu.stream().collect(partitioningBy(Dish::isVegetarian
+        ,groupingBy(Dish::getType)));
+        System.out.println(q1);
+
+        //채식,비채식 요리의 갯수
+        Map<Boolean, Long> q2 = Dish.menu.stream().collect(partitioningBy(Dish::isVegetarian, counting()));
+        System.out.println(q2);
+
+        //채식,비채식 요리중 칼로리 500을 기준으로 나누기
+
+        Map<Boolean, Map<Boolean, List<Dish>>> q3 = Dish.menu.stream().collect(partitioningBy(Dish::isVegetarian,
+                partitioningBy(d->d.getCalories() >500)));
+
+        System.out.println(q3);
+
+
 
 
     }
